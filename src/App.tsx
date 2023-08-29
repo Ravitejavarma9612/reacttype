e1d7de1e9371fe3f6baf7bd3925cc915dce82966
Ravitejavarma9612 <85729052+Ravitejavarma9612@users.noreply.github.com>
2023-08-29 23:13:56 +0530

    Revert "setup is done"

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-
-const App: React.FC = () => {
+function App() {
   return (
-    <Router> 
-      <div>
-        <nav>
-          <ul><Link to="/">Home</Link></ul>
-          <ul><Link to="/about">About</Link></ul>
-          <ul><Link to="/contact" >contact</Link></ul>
-        </nav>
-      </div>
-        <Routes>
-          <Route path="/about" element={<About/>}>
-          </Route>
-      
-          <Route path="/contact" element={<Contact />}>
-          </Route>
-          <Route path="/" element={<Home />}>
-          </Route>
-        </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
